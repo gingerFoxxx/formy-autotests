2357c50a15c71a3cd9345330950aaa18c4cfbc50
gingerFoxxx <gingerFoxxx>
2021-10-20 19:41:20 +0300

    DatePicker add

--- src/test/java/autocomplete/AutocompleteTests.java
+++ src/test/java/autocomplete/AutocompleteTests.java
@@ -16,12 +16,12 @@ public class AutocompleteTests extends BaseTest {
         var autocompletePage = homePage.clickAutocomplete();
         autocompletePage.fillAddressField("10 Cornerstone Squ");
 
-        //assertEquals(autocompletePage.getAddressFieldText(),"10 Cornerstone Square, Уэстфорд, Массачусетс, США","Incorrect text");
+        //assertEquals(autocompletePage.getAddressFieldText(),"10 Cornerstone Square, Westford, MA, USA","Incorrect text");
         assertEquals(autocompletePage.getStreetAddress2FieldText(),"Cornerstone Square","Incorrect text");
         assertEquals(autocompletePage.getCityFieldText(),"Westford","Incorrect text");
         assertEquals(autocompletePage.getStateFieldText(),"MA","Incorrect text");
         assertEquals(autocompletePage.getZipCodeFieldText(),"01886","Incorrect text");
-        assertEquals(autocompletePage.getCountryFieldText(),"Соединенные Штаты Америки","Incorrect text");
+        assertEquals(autocompletePage.getCountryFieldText(),"United States" ,"Incorrect text");
 
         autocompletePage.goBackHomePage();
 
